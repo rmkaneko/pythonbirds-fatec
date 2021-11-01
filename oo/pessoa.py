ef530fd512e31f34b31e93455306f7bd9dab5ff2
rmkaneko <roberto_kaneko@hotmail.com>
2021-10-31 22:08:16 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,16 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=54):
+        self.nome = nome
+        self.idade = idade
+
+
     def cumprimentar(self):
         return f'Olá'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Luciano')
     print(p.cumprimentar())
+    p.nome = 'Roberto'
+    print(p.nome)
+    print(p.idade)
